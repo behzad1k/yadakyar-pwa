5f219467c67d5bad9e95b28000c6215ce03d31f1
behzad1k <bhzd1k@gmail.com>
2023-12-04 17:53:31 +0330

    fix home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { fetchData } from '@/app/builder';
 import Home from '@/app/home/page';
+import HomeHeader from '@/components/header/HomeHeader';
 import Nav from "@/components/layout/Nav";
 import Splash from '@/components/layout/Splash';
 
 const App = () => {
 
-  return <Home />
+  return (
+    <>
+      <HomeHeader />
+      <Home />
+      <Nav/>
+    </>
+  )
 };
 
 export default App;
